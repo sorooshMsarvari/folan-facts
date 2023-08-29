6f04cbf6b857fa6f86eb1546dcab68febb861279
Soroosh Mirzasarvari <sarvisoroosh@gmail.com>
2023-08-29 22:47:30 +0330

    refactor folan.sh

--- folan.sh
+++ folan.sh
@@ -1,27 +1,62 @@
 #!/bin/bash
+set -euo pipefail
 
-NEED_DESC=false
+function cd_here() {
+  cd $(dirname -- $BASH_SOURCE)
+}
 
-while [[ $# -gt 0 ]]; do
-  case $1 in
-    -d|--description)
-      NEED_DESC=true
-      shift # past argument
-      ;;
-    *)
-      shift # past argument
-      ;;
-  esac
-done
+function get_args() {
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      -d|--description)
+        NEED_DESC=true
+        shift # past argument
+        ;;
+      *)
+        shift # past argument
+        ;;
+    esac
+  done
+}
 
-FACT_NUM=$(cat folan-facts.json | jq '. | length')
+function get_fact_num() {
+  cat $FACTS_FILE | jq '. | length'
+}
 
-FACT_INDEX=$(($RANDOM % $FACT_NUM))
-FACT=$(cat folan-facts.json | jq -r --arg FACT_INDEX $FACT_INDEX '.[$FACT_INDEX | tonumber].fact')
-echo $FACT
+function get_random_fact() {
+  FACT_NUM=$(get_fact_num)
+  FACT_INDEX=$((RANDOM % FACT_NUM))
+  jq -r --arg f_idx $FACT_INDEX '.[$f_idx | tonumber]' "$FACTS_FILE"
+}
 
-if $NEED_DESC
-then
-  DESC=$(cat folan-facts.json | jq -r --arg FACT_INDEX $FACT_INDEX '.[$FACT_INDEX | tonumber].desc')
+
+function print_fact() {
+  local FACT=$(echo "$1" | jq -r '.fact')
+  echo $FACT
+}
+
+function print_desc() {
+  local DESC=$(echo "$1" | jq -r '.desc')
   echo $DESC
-fi
+}
+
+function print_random_fact() {
+  FACT_OBJ=$(get_random_fact)
+  print_fact "$FACT_OBJ"
+  if $NEED_DESC
+  then
+    print_desc "$FACT_OBJ"
+  fi
+}
+
+function main() {
+  NEED_DESC=false
+  FACTS_FILE="folan-facts.json"
+
+  cd_here
+  get_args "$@"
+
+  print_random_fact
+}
+
+main "$@"
